f911ccae7f370ce548f7ecbdc2007fc90a87a80f
Sayed Nahid <nahid15-3849@diu.edu.bd>
2025-06-11 12:04:29 +0600

    included firebase with our existing project

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:g_chat/pages/dashboard_screen.dart';
 import 'package:g_chat/pages/profile_set_screen.dart';
 import 'package:g_chat/pages/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
